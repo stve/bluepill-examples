08c2452e1608425e4f87dacb016e6dfc869be756
Steve Agalloco <steve.agalloco@gmail.com>
2010-05-07 01:45:38 -0400

    added file watch

---
+++ bluepill_file_watch_pc.rb
@@ -0,0 +1,31 @@
+# gist: http://gist.github.com/390596
+
+Bluepill.define_process_condition(:log_watch) do
+
+  def initialize(options = {})
+    @file_name = options.delete(:file)
+    @regexp = options.delete(:regexp)
+    @file_pos = 0
+    @file_size = 0
+  end
+
+  def run(pid)
+    File.open(@file_name, "r") do |file|
+      @file_size = file.stat.size
+      @file_pos = 0 if @file_size < @file_pos # file truncated
+      file.seek @file_pos, IO::SEEK_SET
+      @file_pos = @file_size # grep will seek to end
+      file.grep(@regexp)
+    end
+  end
+  
+  def check(value)
+    value.empty?
+  end
+
+end
+
+
+# --------------------
+
+process.checks :log_watch, :file => "file.log", :regexp => /ErrorXPTO/, :every => 60
